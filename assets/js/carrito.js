9296d6dded40f5e5d464515ee32fbaa48e5929c2
robert Salazar <11rsahome@gmail.com>
2019-11-20 17:11:50 +0100

    peticion js json y maquetando Boostrap

---
+++ assets/js/carrito.js
@@ -0,0 +1,33 @@
+var baseDeDatos = [{
+        id: 1,
+        marca: 'ZapatoCheck',
+        nombre: 'Casual',
+        img: 'assets/img/carro.png',
+        precio: 500,
+
+    },
+    {
+        id: 2,
+        marca: 'Zapatillas',
+        nombre: 'Deportiva',
+        precio: 2000
+    },
+    {
+        id: 3,
+        marca: 'Zona',
+        nombre: 'ThinkPadX1',
+        precio: 1000
+    },
+    {
+        id: 4,
+        marca: 'Zico',
+        nombre: 'Predator',
+        precio: 500
+    },
+    {
+        id: 5,
+        marca: 'Escot',
+        nombre: 'Fast',
+        precio: 800
+    }
+];
